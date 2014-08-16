27eb646ebf72721b2e455512f9e5245b8f34dd93
Tyler Henkel <tylerhenkel@hotmail.com>
2014-08-16 00:48:25 -0600

    Built using Angular Fullstack v2.0.9 from commit 849a937

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -44,13 +44,13 @@ var all = {
   twitter: {
     clientID:     process.env.TWITTER_ID || 'id',
     clientSecret: process.env.TWITTER_SECRET || 'secret',
-    callbackURL:  process.env.DOMAIN + '/auth/twitter/callback'
+    callbackURL:  process.env.DOMAIN || '' + '/auth/twitter/callback'
   },
 
   google: {
     clientID:     process.env.GOOGLE_ID || 'id',
     clientSecret: process.env.GOOGLE_SECRET || 'secret',
-    callbackURL:  process.env.DOMAIN + '/auth/google/callback'
+    callbackURL:  process.env.DOMAIN || '' + '/auth/google/callback'
   }
 };
 
